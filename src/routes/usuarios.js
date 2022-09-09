249b170a2f9a3a989f51aa8ecd08a35e97f4fca1
Baltasar Barbaglia <baltasarbarbaglia@gmail.com>
2022-09-08 23:48:13 -0300

    Commit 1

---
+++ src/routes/usuarios.js
@@ -0,0 +1,46 @@
+import { Router } from 'express'
+import passport from '../controller/PassportController.js'
+import  * as userController from '../controller/UsuariosController.js'
+import {mdwValidateSchemaNewUsuario} from "../middleware/usuariosMDW.js"
+import {uploadUsers} from "../multer.js"
+
+const UsersRoutes = new Router();
+
+//GET '/' --> returns all users 
+UsersRoutes.get('/allUsers', userController.obtenerUsuarios);
+
+//POST /registro --> create a new user
+UsersRoutes.post('/registro',  
+    mdwValidateSchemaNewUsuario,
+    passport.authenticate('registro', {
+    failureRedirect: '/failRegister'}),
+    userController.successRegister
+);
+UsersRoutes.get('/failRegister', userController.failRegister);
+UsersRoutes.post('/failRegister', userController.failRegister);
+UsersRoutes.get('/successRegister', userController.successRegister);
+UsersRoutes.post('/successRegister', userController.successRegister);
+
+//POST '/login' --> validate the login, receive email and password of the user
+UsersRoutes.post('/login', 
+    passport.authenticate('login', {failureRedirect: '/failLogin'}),
+    userController.successLogin
+);
+UsersRoutes.post('/failLogin', userController.failLogin);
+UsersRoutes.get('/failLogin', userController.failLogin);
+UsersRoutes.post('/successLogin', userController.successLogin);
+UsersRoutes.get('/successLogin', userController.successLogin);
+
+//GET '/logout' --> logs out the user
+UsersRoutes.get('/logout', userController.logout);
+
+//DELETE /deleteUser/{emailUser} --> to delete a user by email
+UsersRoutes.delete('/deleteUser/:email', userController.borrarUsuario);
+
+//SUBIR ARCHIVOS
+UsersRoutes.post('/uploadFile', 
+    uploadUsers.single("image"), 
+    userController.uploadFile
+  )
+
+export default UsersRoutes 
